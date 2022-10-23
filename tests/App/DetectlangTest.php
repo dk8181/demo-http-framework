a66a7c6fee42ad1410a8af6ed1d67ac849c03acc
dk8181 <dka8080@mail.ru>
2022-10-23 18:40:49 +0300

    Added other tests

--- tests/App/DetectlangTest.php
+++ tests/App/DetectlangTest.php
@@ -11,7 +11,7 @@ use function App\detectLang;
 /**
  * @internal description
  */
-final class DetectLangTEst extends TestCase
+final class DetectLangTest extends TestCase
 {
     public function testDefault(): void
     {
@@ -21,19 +21,59 @@ final class DetectLangTEst extends TestCase
         self::assertEquals('en', $lang);
     }
 
-    public function testQueryParams(): void
+    public function testQueryParam(): void
     {
-        $request = ['queryParams' => ['lang' => 'de']];
+        $request = [
+            'queryParams' => ['lang' => 'de'],
+            'parsedBody' => ['lang' => 'fr'],
+            'cookieParams' => ['lang' => 'pt'],
+            'headers' => ['Accept-Language' => 'ru-ru,ru;q=0.8,en;q=0.4'],
+        ];
+
         $lang = detectLang($request, 'en');
 
         self::assertEquals('de', $lang);
     }
 
-    public function testBodyParams(): void
+    public function testBodyParam(): void
     {
-        $request = ['parsedBody' => ['lang' => 'fr']];
+        $request = [
+            'queryParams' => [],
+            'parsedBody' => ['lang' => 'fr'],
+            'cookieParams' => ['lang' => 'pt'],
+            'headers' => ['Accept-Language' => 'ru-ru,ru;q=0.8,en;q=0.4'],
+        ];
+
         $lang = detectLang($request, 'en');
 
         self::assertEquals('fr', $lang);
+    }
+
+    public function testCookie(): void
+    {
+        $request = [
+            'queryParams' => [],
+            'parsedBody' => [],
+            'cookieParams' => ['lang' => 'pt'],
+            'headers' => ['Accept-Language' => 'ru-ru,ru;q=0.8,en;q=0.4'],
+        ];
+
+        $lang = detectLang($request, 'en');
+
+        self::assertEquals('pt', $lang);
+    }
+
+    public function testHeader(): void
+    {
+        $request = [
+            'queryParams' => [],
+            'parsedBody' => [],
+            'cookieParams' => [],
+            'headers' => ['Accept-Language' => 'ru-ru,ru;q=0.8,en;q=0.4'],
+        ];
+
+        $lang = detectLang($request, 'en');
+
+        self::assertEquals('ru', $lang);
     }
 }
