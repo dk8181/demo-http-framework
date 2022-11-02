aadcda819db534dcca27e5d7998e3a06fa204b0f
dk8181 <dka8080@mail.ru>
2022-11-02 08:45:00 +0300

    Extracted to home function language feature

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Framework\Http\Message\ServerRequest;
+
 use function App\detectLang;
 use function Framework\Http\createServerRequestFromGlobals;
 
@@ -10,18 +12,24 @@ http_response_code(500);
 /** @psalm-suppress MissingFile */
 require __DIR__ . './../vendor/autoload.php';
 
-$request = createServerRequestFromGlobals();
+function home(ServerRequest $request)
+{
+    $name = $request->getQueryParams()['name'] ?? 'Guest';
 
-$name = $request->getQueryParams()['name'] ?? 'Guest';
+    if (!is_string($name)) {
+        http_response_code(400);
 
-if (!is_string($name)) {
-    http_response_code(400);
-    exit;
+        return;
+    }
+
+    $lang = detectLang($request, 'en');
+
+    http_response_code(200);
+    header('Content-Type: text/plain; charset=utf-8');
+    header('X-Frame-Options: DENY');
+    echo 'Hello, ' . $name . '! I am gussing your language is ' . $lang;
 }
 
-$lang = detectLang($request, 'en');
+$request = createServerRequestFromGlobals();
 
-http_response_code(200);
-header('Content-Type: text/plain; charset=utf-8');
-header('X-Frame-Options: DENY');
-echo 'Hello, ' . $name . '! I am gussing your language is ' . $lang;
+home($request);
